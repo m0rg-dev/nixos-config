0781da028a2565d298e009acc9b68e2f13a9516e
Morgan Wolfe <corp@m0rg.dev>
2022-06-06 07:29:29 -0700

    add gnome-tweaks to GUI profile

--- profiles/gui.nix
+++ profiles/gui.nix
@@ -3,6 +3,7 @@
 {
   config = {
     globals.graphical = true;
-    home.packages = [ pkgs.discord pkgs.firefox pkgs.iosevka ];
+    home.packages =
+      [ pkgs.discord pkgs.firefox pkgs.iosevka pkgs.gnome3.gnome-tweaks ];
   };
 }
