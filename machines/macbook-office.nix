89200fe790cf26b4c29b0fb573db961b392fa1f1
Morgan Wolfe <m@m0rg.dev>
2022-11-09 16:26:26 -0800

    fix work laptop

--- machines/macbook-office.nix
+++ machines/macbook-office.nix
@@ -4,8 +4,24 @@
   imports = [ ./base.nix ../profiles/osx.nix ];
 
   home.packages = [
+    pkgs.awscli2
+    pkgs.gopls
     pkgs.postman
   ];
+
+  programs.go = {
+    enable = true;
+    package = pkgs.go_1_19;
+  };
+
+  programs.vscode = {
+    enable = true;
+    extensions = with pkgs.vscode-extensions; [
+      bbenoist.nix
+      arrterian.nix-env-selector
+      golang.go
+    ];
+  };
 
   home.username = "morgan";
   home.homeDirectory = "/Users/morgan";
