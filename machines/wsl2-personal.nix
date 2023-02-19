d1164089e1989ad211423c8e446e543e8b12537e
Morgan Wolfe <corp@m0rg.dev>
2023-02-19 16:25:03 +0000

    run nixfmt

--- machines/wsl2-personal.nix
+++ machines/wsl2-personal.nix
@@ -3,13 +3,11 @@
 {
   imports = [ ./base.nix ];
 
-	globals.wsl2 = true;
+  globals.wsl2 = true;
 
   home.username = "morgan";
   home.homeDirectory = "/home/morgan";
   home.stateVersion = "21.11";
-	
-	home.packages = [
-		pkgs.util-linux
-	];
+
+  home.packages = [ pkgs.util-linux ];
 }
