3fed19d63c43020d72c952ef7a754709d59c460c
Morgan Wolfe <corp@m0rg.dev>
2023-02-28 08:17:33 -0800

    vscode config

--- modules/vscode.nix
+++ modules/vscode.nix
@@ -10,6 +10,16 @@
       rust-lang.rust-analyzer
       stkb.rewrap
       tamasfe.even-better-toml
+      usernamehw.errorlens
     ];
+    userSettings = {
+      "editor.formatOnSave" = true;
+      "html.autoCreateQuotes" = false;
+      "html.autoClosingTags" = false;
+      "rust-analyzer.checkOnSave" = true;
+      "rust-analyzer.check.command" = "clippy";
+      "editor.fontFamily" = "'Berkeley Mono', Menlo, Monaco, 'Courier New', monospace";
+      "workbench.colorTheme" = "Kimbie Dark";
+    };
   };
 }
