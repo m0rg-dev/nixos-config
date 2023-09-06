91c1033103e0f6d83ddf5806ae3edfe0f40e287e
Morgan Wolfe <m@m0rg.dev>
2023-09-05 22:07:55 -0700

    vscode smaller on macbook

--- machines/macbook.nix
+++ machines/macbook.nix
@@ -5,6 +5,8 @@
 
   globals.git_signing_key = "${config.home.homeDirectory}/.ssh/id_rsa.pub";
 
+  programs.vscode.userSettings."window.zoomLevel" = -1;
+
   home.username = "morgan";
   home.homeDirectory = "/Users/morgan";
   home.stateVersion = "21.11";
